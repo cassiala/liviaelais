eeb5a52c4aea8bfbb6bb621f4ced1c4ca868277a
cassiala <lais.lourenco.costa@escola.pr.gov.br>
2024-11-07 08:34:06 -0300

    ff

--- pagina2.js
+++ pagina2.js
@@ -3,7 +3,7 @@ function criaCartao (categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="conteudo-cartao">
+    <div class="conteudo-cartao" onclick = "mostraReposta (this)">
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
             <p>${pergunta}</p>
@@ -15,4 +15,14 @@ function criaCartao (categoria, pergunta, resposta){
          `
   
          container.appendChild(cartao);
+    }
+
+    function mostraReposta (card){
+        const resposta = card.querySelector('.resposta-cartao');
+        if(resposta.style.display === 'none'){
+            resposta.style.diplay = 'block'
+        }
+        else{
+            resposta.style.display = 'none'
+        }
     }
